97341e779eca53c8c87b6e7e152487f2046f8424
abbiehuynh <abbiehuynhh@gmail.com>
2024-08-05 14:25:31 -0500

    adds new array and ability to iterate through array of objects

--- mini-project/event.js
+++ mini-project/event.js
@@ -6,3 +6,37 @@ class Event {
         this.availableTickets = [];
     }
 }
+
+// creates objects and assigns values to them
+const eventObj1 = new Event (
+    'KLOS Golden Gala',
+    'An evening with hollywood vampires'
+);
+
+const eventObj2 = new Event(
+    'Skillet & Sevendust', 
+    'Victorious war tour'
+);
+
+const eventObj3 = new Event(
+    'Jenny Lewis', 
+    'On the line tour 2019'
+);
+
+// creates an empty Event array
+const eventArray = new Array();
+
+//pushes the objects created into the array 
+eventArray.push(eventObj1, eventObj2, eventObj3);
+
+console.log(eventArray);
+
+document.addEventListener('DOMContentLoaded', () => {
+    let html = "";
+    // iterates through the array of objects
+    eventArray.forEach((item) => {
+        html += `<li>${item.name} - ${item.description}`;
+    });
+    document.querySelector('#event').innerHTML = html;
+});
+
