57ce7cdc7456e9660fd736f0c155689e5ef47fe9
abbiehuynh <abbiehuynhh@gmail.com>
2024-08-05 16:17:08 -0500

    adds flip coin function and images for both sides of coin

--- coin-flip/index.js
+++ coin-flip/index.js
@@ -2,11 +2,59 @@
 let canvas = document.getElementById("myCanvas");
 let c = canvas.getContext("2d");
 
-// declare variables W and H = width and height of canvas
+// declare variables of width and height of canvas
+let W = canvas.width;
+let H = canvas.height;
 
 // flip Coin function
 function flipCoin() {
 
-    // declare variable flip; assign random number: 0 - 14
-        // 0 - heads, 1 - tails
+    // declare variable flip; assign random number: 0 - 1 (0 - heads, 1 - tails)
+    // Math.floor => decimal to whole number; Math.random * 2 => random value between 0-1
+    let flip = Math.floor(Math.random() * 2)
+    // logs flip to console
+    console.log(flip);
+    // if/else statement
+    if (flip === 0)
+    {
+        drawHeads(200, 200, "Gold");
+    } else {
+        drawTails(400, 200, "Silver");
+    }
 }
+ // drawHeads function
+ function drawHeads(xLoc, yLoc, color) {
+    // draws head side of coin, draws circle, radius = 100;
+    c.beginPath();
+    c.fillStyle = color;
+    c.strokeStyle = "Black";
+    c.lineWidth = 5;
+    c.arc(xLoc, yLoc, 100, 0, 2 * Math.PI);
+    c.fill();
+    c.stroke();
+    c.closePath();
+
+    //draw a black "H" on the coin
+    c.fillStyle = "Black";
+    c.font = "72px Arial";
+    c.fillText("H", xLoc, yLoc);
+ }
+
+
+// drawTails function
+ function drawTails(xLoc, yLoc, color) {
+    // draws tail side of coin, draws circle, radius = 100;
+    c.beginPath();
+    c.fillStyle = color;
+    c.strokeStyle = "Black";
+    c.lineWidth = 5;
+    c.arc(xLoc, yLoc, 100, 0, 2 * Math.PI);
+    c.fill();
+    c.stroke();
+    c.closePath();
+
+    //draw a black "T" on the coin
+    c.fillStyle = "Black";
+    c.font = "72px Arial";
+    c.fillText("T", xLoc, yLoc);
+ }
