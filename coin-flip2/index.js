939d2fa9271e74e4a5bd0ae386a7e2a11bfab4a5
abbiehuynh <abbiehuynhh@gmail.com>
2024-08-06 10:50:15 -0500

    adds event listener to flip button

--- coin-flip2/index.js
+++ coin-flip2/index.js
@@ -1,16 +1,39 @@
 // coin-flip game
 
-// creates variables for heads, tails, coin
-let heads = 0;
-let tails = 1;
+// creates variables to access elements
+let heads = document.getElementById("heads");
+let tails = document.getElementById("tails");
 
+let coin = document.getElementsByClassName("coin");
 
-// set value of heads and tails 0, 1
-
-// function generates random value between 0-1 to choose heads or tails
-
-// function runs onClick of button
+let flipButton = document.getElementById("flip-button");
+let resetButton = document.getElementById("reset-button");
 
 // counter keeps track of number of heads and tails
+let headsCount = 0;
+let tailsCount = 0;
+
+// adds event listener to flip button
+flipButton.addEventListener("click", (event) => {
+    
+    // function generates random value between 0-1 to choose heads or tails
+    let flip = Math.floor(Math.random() * 2);
+    console.log(flip);
+    if (flip === 0) {
+        drawHeads();
+    } else {
+        drawTails();
+    }
+});
+
+function drawHeads () {
+    heads.style.visibility = "visible";
+    tails.style.visibility = "hidden";
+}
+
+function drawTails () {
+    heads.style.visibility = "hidden";
+    tails.style.visibility = "visible";
+}
 
 // button to reset counter 
