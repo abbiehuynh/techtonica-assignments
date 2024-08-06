4942fd1f31f9aa403639cc00a8a7403c733bfdef
abbiehuynh <abbiehuynhh@gmail.com>
2024-08-05 22:35:02 -0500

    adds general psuedocode

---
+++ coin-flip2/index.js
@@ -0,0 +1,13 @@
+// coin-flip game
+
+// creates variables for heads, tails, coin
+
+// set value of heads and tails 0, 1
+
+// function generates random value between 0-1 to choose heads or tails
+
+// function runs onClick of button
+
+// counter keeps track of number of heads and tails
+
+// button to reset counter 
