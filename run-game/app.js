c2eac4ec5d81bba22e054ec23df8f471f9d7fbd2
abbiehuynh <abbiehuynhh@gmail.com>
2024-08-07 22:10:15 -0500

    adds moving down function; prevents double jumping

--- run-game/app.js
+++ run-game/app.js
@@ -6,29 +6,65 @@ document.addEventListener("DOMContentLoaded", function() {
     const dino = document.querySelector(".dino");
     const grid = document.querySelector(".grid");
 
-    let gravity = 0.9;
+    // creates variable for jump function
+    let gravity = 1;
+    let isJumping = false;
     
     function control(e) {
         // create connection to space bar, when hit, the jump function will run
         if (e.code === "Space") {
-            console.log("jump")
-            jump()
+
+            // prevents user from double jumping, only able to jump when they are not jumping
+            if (!isJumping) {
+                jump()
+            }
         }
     }
+
     let position = 0;
+    
+
     // jump function
     function jump() {
+
+        isJumping = true;
+        let count = 0;
         let timerId = setInterval(function() {
-        
-            // move up
-            position =+ 500;
+             
+            // moves down
+            if (count === 15) {
+                clearInterval(timerId)
+                let downTimerId = setInterval(function() {
+
+                    // stops from infinitely moving down webpage
+                    if (count === -12) {
+                        clearInterval(downTimerId)
+                        isJumping = false;
+                    }
+                    // creates visual effect of moving down
+                    position -= 5;
+                    count--;
+                    position = position * gravity;
+
+                    // overrides position in css
+                    dino.style.bottom = position + "px";
+
+                // invokes for every 20 milliseconds
+                }, 20);
+            }
+
+            // moves up
+            position =+ 180;
+            count++;
             position = position * gravity;
 
             // overrides position in css
-            dino.style.bottom = position + 'px'
-
-        // invokes for every 20 milliseconds
+            dino.style.bottom = position + 'px';
         }, 20)
     }
+
+
+
+
     document.addEventListener('keydown', control)
 });
